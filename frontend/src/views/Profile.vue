44cae4c13489208146b5b1bea93442fcd676eacb
Ramon Cândido <ramoncandido64@gmail.com>
2025-02-19 03:43:39 -0300

    Criada pagina de perfil do usuario

---
+++ frontend/src/views/Profile.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Perfil do Usuário</h1>
+    <p>Aqui você pode ver e editar suas informações de perfil.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PersonalProfile',
+};
+</script>
