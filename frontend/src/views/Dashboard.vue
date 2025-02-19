b6da9a60c0b5e75829f3198d160eaab2f4667a03
Ramon Cândido <ramoncandido64@gmail.com>
2025-02-19 01:40:29 -0300

    Criada pagina de dashboard

---
+++ frontend/src/views/Dashboard.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Dashboard</h1>
+    <p>Bem-vindo ao seu dashboard financeiro!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DashboardFinanceiro',
+};
+</script>
