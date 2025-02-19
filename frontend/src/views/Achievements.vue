8c7b1f678eeb36834cd47d2383c043710c399ee0
Ramon Cândido <ramoncandido64@gmail.com>
2025-02-19 01:40:10 -0300

    Criada pagina de conquista

---
+++ frontend/src/views/Achievements.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Conquistas</h1>
+    <p>Veja suas conquistas financeiras aqui.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ConquistasGlobais',
+};
+</script>
