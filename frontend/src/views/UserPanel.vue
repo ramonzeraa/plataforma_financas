e90fb2c58810ae395715117c4ca01689a8dbc05f
Ramon Cândido <ramoncandido64@gmail.com>
2025-02-19 01:40:52 -0300

    Criada pagina de perfil do usuario

---
+++ frontend/src/views/UserPanel.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Painel de Usuário</h1>
+    <p>Gerencie suas informações de usuário aqui.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserPanel',
+};
+</script>
