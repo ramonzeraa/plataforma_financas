64c34b41052eeebb49fa2f98f53fe95b14eee119
Ramon Cândido <ramoncandido64@gmail.com>
2025-02-19 03:43:19 -0300

    Criada pagina de login

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>Login</h1>
+    <form @submit.prevent="login">
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input
+          type="email"
+          class="form-control"
+          id="email"
+          v-model="email"
+          required
+        />
+      </div>
+      <div class="form-group">
+        <label for="password">Senha</label>
+        <input
+          type="password"
+          class="form-control"
+          id="password"
+          v-model="password"
+          required
+        />
+      </div>
+      <button type="submit" class="btn btn-primary">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageLogin',
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+  methods: {
+    login() {
+      // Implementar a lógica de login aqui
+      this.$router.push('/dashboard');
+    },
+  },
+};
+</script>
