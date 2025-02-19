728627ec126c5277decef49e67a91aa7b8a85038
Ramon Cândido <ramoncandido64@gmail.com>
2025-02-19 01:39:29 -0300

    Criada a navbar da plataforma

---
+++ frontend/src/components/NavbarAll.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Finanças Pessoais</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link class="nav-link" to="/dashboard">Dashboard</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/user-panel"
+            >Painel de Usuário</router-link
+          >
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/achievements"
+            >Conquistas</router-link
+          >
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavbarGeral',
+};
+</script>
+
+<style scoped>
+.navbar {
+  margin-bottom: 20px;
+}
+</style>
